910960759fd7ecfa6b943a14913232bf575664df
Achilles Charmpilas <ac@humbuckercode.co.uk>
2013-01-03 00:53:34 +0100

    the require failed n trvais

--- lib/piwik.rb
+++ lib/piwik.rb
@@ -2,8 +2,7 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'active_support/core_ext'
-Dir[File.join(File.dirname(__FILE__),'piwik','*.rb')].each { |r| require r }
+%W{ base version site user }.each { |r| require "piwik/#{r}" }
 
 module Piwik
-  # Your code goes here...
 end
