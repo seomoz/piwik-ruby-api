90921aeff43fa877a470896b021f262e2e0fdc44
Achilles Charmpilas <ac@humbuckercode.co.uk>
2013-01-26 18:25:22 +0100

    added and error and a null spec for the example api

--- spec/example_api_spec.rb
+++ spec/example_api_spec.rb
@@ -9,4 +9,7 @@ describe 'Piwik::ExampleApi' do
   subject { Piwik::ExampleApi }
   
   assert_value_integrity(:get_piwik_version, :value => '1.10.1')
+  assert_value_integrity(:get_null, :value => nil)
+  
+  it { expect { subject.get_object }.to raise_error(Piwik::ApiError) }
 end
